993d3908859879747625596057d9f8be0f1784f5
tam03 <tamuramura2803@gmail.com>
2020-07-11 12:38:28 +0900

    -lオプション対応

--- wc.rb
+++ wc.rb
@@ -3,26 +3,41 @@
 require 'optparse'
 
 class WcCommand
+  def initialize
+    opt = OptionParser.new
+    @params = ARGV.getopts('l')
+    @file = opt.parse!(ARGV)
+  end
   def standard_input_or_file
-    @stdin = StandardInput.new if ARGV[0].nil? == true
-    @input = InputtedFile.new if ARGV[0].nil? == false
+    @stdin = StandardInput.new if @file[0].nil? == true
+    @input = InputtedFile.new if @file[0].nil? == false
   end
   def run
-    if ARGV[0].nil? == true
+    if @file[0].nil? == true
       stdin_detail = [
         @stdin.generate_number_of_lines,
         @stdin.generate_word_count,
         @stdin.byte_size,
       ]
-      View.new(stdin_detail)
-    elsif ARGV[0].nil? == false
+      if @params['l']
+        View.new.print_wc(stdin_detail, 1)
+      else
+        View.new.print_wc(stdin_detail, 2)
+      end
+    elsif @file.size == 1
       input_detail = [
         @input.generate_number_of_lines,
         @input.generate_word_count,
         @input.byte_size,
         @input.file_name,
       ]
-      View.new(input_detail)
+      if @params['l']
+        View.new.print_wc(input_detail, 1)
+      else
+        View.new.print_wc(input_detail, 2)
+      end
+    elsif @file.size <= 2
+      MultiInpttedFile.new.multi_file_view
     end
   end
 end
@@ -32,7 +47,7 @@ class StandardInput
     @standard_input = $stdin.read#.map {|r| r.split(/\s/) }
   end
   def generate_number_of_lines
-    p @standard_input.split(/\R/).size
+    @standard_input.split(/\R/).size
   end
   def generate_word_count
     b = @standard_input.split("\n").size
@@ -45,33 +60,58 @@ class StandardInput
 end
 
 class InputtedFile
-  def initialize
-    @inputted_file = ARGV[0]
-  end
-  def generate_number_of_lines
-    open(@inputted_file){|f|
+  def generate_number_of_lines(inputted_file = ARGV[0])
+    open(inputted_file){|f|
       while f.gets; end
       f.lineno
     }#行数
   end
-  def generate_word_count
-    file = open(@inputted_file).read
+  def generate_word_count(inputted_file = ARGV[0])
+    file = open(inputted_file).read
     s = file.gsub(/\n/, ' ')
     s.split(" ").size
   end
-  def byte_size
-    File.stat(@inputted_file).size
+  def byte_size(inputted_file = ARGV[0])
+    File.stat(inputted_file).size
   end
-  def file_name
-    @inputted_file
+  def file_name(inputted_file = ARGV[0])
+    inputted_file
   end
 end
 
-class View
-  def initialize(array)
-    array.each {|view|
-      print view.to_s.rjust(8)
+class MultiInpttedFile
+  def multi_file_view
+    lines = []
+    words = []
+    bytes = []
+    total = []
+    ARGV.map {|file_name| 
+      input = InputtedFile.new
+      input_detail = [
+        input.generate_number_of_lines(file_name),
+        input.generate_word_count(file_name),
+        input.byte_size(file_name),
+        input.file_name(file_name),
+      ]
+      View.new.print_wc(input_detail)
+      lines << input_detail[0]
+      words << input_detail[1]
+      bytes << input_detail[2]
     }
+    total = [lines.sum, words.sum, bytes.sum, "total"]
+    View.new.print_wc(total)
+  end
+end
+
+class View < WcCommand
+  def print_wc(array, option)
+    if option == 1
+      print array[0].to_s.rjust(8)
+    elsif
+      array.each {|view|
+        print view.to_s.rjust(8)
+      }
+    end
   end
 end
 
@@ -80,4 +120,3 @@ wc.standard_input_or_file
 wc.run
 
 # 本家 ls -l | wc の結果  行24     単語209    バイト1499
-
