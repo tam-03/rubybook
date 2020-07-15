dbd41d1d7d8aff6dc5237186a6e3d5d8a10f3b5f
tam03 <tamuramura2803@gmail.com>
2020-07-15 11:41:13 +0900

    rubocop

--- wc.rb
+++ wc.rb
@@ -8,16 +8,18 @@ class WcCommand
     @params = ARGV.getopts('l')
     @file = opt.parse!(ARGV)
   end
+
   def standard_input_or_file
     @stdin = StandardInput.new if @file[0].nil? == true
     @input = InputtedFile.new if @file[0].nil? == false
   end
+
   def run
     if @file[0].nil? == true
       stdin_detail = [
         @stdin.generate_number_of_lines,
         @stdin.generate_word_count,
-        @stdin.byte_size,
+        @stdin.byte_size
       ]
       if @params['l']
         View.new.print_wc(stdin_detail, 1)
@@ -29,7 +31,7 @@ class WcCommand
         @input.generate_number_of_lines,
         @input.generate_word_count,
         @input.byte_size,
-        @input.file_name,
+        @input.file_name
       ]
       if @params['l']
         View.new.print_wc(input_detail, 1)
@@ -40,14 +42,13 @@ class WcCommand
       lines = []
       words = []
       bytes = []
-      total = []
-      ARGV.map {|file_name| 
+      ARGV.map do |file_name|
         input = InputtedFile.new
         input_detail = [
           input.generate_number_of_lines(file_name),
           input.generate_word_count(file_name),
           input.byte_size(file_name),
-          input.file_name(file_name),
+          input.file_name(file_name)
         ]
         if @params['l']
           View.new.print_wc(input_detail, 1)
@@ -57,8 +58,8 @@ class WcCommand
         lines << input_detail[0]
         words << input_detail[1]
         bytes << input_detail[2]
-      }
-      total = [lines.sum, words.sum, bytes.sum, "total"]
+      end
+      total = [lines.sum, words.sum, bytes.sum, 'total']
       if @params['l']
         View.new.print_wc(total, 1)
       else
@@ -72,14 +73,17 @@ class StandardInput
   def initialize
     @standard_input = $stdin.read
   end
+
   def generate_number_of_lines
     @standard_input.split(/\R/).size
   end
+
   def generate_word_count
     words = @standard_input.unpack('H*')
     words_count = words[0].gsub(/20|0c|0a|0d|09|0b|a0|a0/, ' ')
-    words_count.split(" ").size
+    words_count.split(' ').size
   end
+
   def byte_size
     @standard_input.bytesize
   end
@@ -89,14 +93,17 @@ class InputtedFile
   def generate_number_of_lines(inputted_file = ARGV[0])
     File.read(inputted_file).count("\n")
   end
+
   def generate_word_count(inputted_file = ARGV[0])
     file = open(inputted_file).read
     s = file.gsub(/\n/, ' ')
-    s.split(" ").size
+    s.split(' ').size
   end
+
   def byte_size(inputted_file = ARGV[0])
     File.stat(inputted_file).size
   end
+
   def file_name(inputted_file = ARGV[0])
     inputted_file
   end
@@ -106,11 +113,10 @@ class View
   def print_wc(file_or_stdin_array, option)
     if option == 1
       print " #{file_or_stdin_array[0].to_s.rjust(7)} #{file_or_stdin_array[3]}"
-    elsif
-      file_or_stdin_array.each {|view|
-        print " #{view.to_s.rjust(7)}"
-      }
-      puts ""
+    elsif file_or_stdin_array.each do |view|
+            print " #{view.to_s.rjust(7)}"
+          end
+      puts ''
     end
   end
 end
